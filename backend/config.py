c9e5c45b625e3df9939a9bf6bba52f052e885f65
greyishsong <2199143566@qq.com>
2020-11-26 22:57:22 +0800

    fix the bug of incorrect size limit(from 30MiB to 50MiB)

--- backend/config.py
+++ backend/config.py
@@ -1,4 +1,4 @@
 SECRET_KEY='fileshare'
 UPLOAD_FOLDER='/var/www/fileshare'
-MAX_CONTENT_LENGTH=30*1024*1024
+MAX_CONTENT_LENGTH=50*1024*1024
 LOG_FILE='/var/www/fileshare/exchange.log'
---
+++ guni.py
@@ -0,0 +1,4 @@
+bind = '127.0.0.1:5001'
+workers = 2
+timeout = 10
+keepalive = 5
