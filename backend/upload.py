2a57197c153819580bbf0120cc3090cb3be8f19c
greyishsong <2199143566@qq.com>
2020-11-27 10:38:48 +0800

    add code for authority, having not been tested yet

--- backend/upload.py
+++ backend/upload.py
@@ -2,40 +2,57 @@ import os
 import time
 import functools
 from flask import (
-        Blueprint, flash, request, render_template, abort, jsonify, url_for, current_app
+        Blueprint, request, abort, jsonify, url_for, current_app
         )
 from urllib.parse import unquote
 from backend.db import get_db
 
 bp = Blueprint('upload', __name__)
 
+# Provide authorization through a unique keyfile for every user and MAC address
+# Originally no MAC address is related to a keyfile, once the first request
+# with it comes, the MAC address of in the request will be related with it, and
+# the keyfile is bounded with this MAC address permanently.
+# The route '/upload' only receive POST request for uploading files, any GET
+# request will be rejected with a '403 Forbidden'.
 @bp.route('/upload', methods=('GET', 'POST'))
 def upload():
     if request.method == 'POST':
         print("receive POST request")
+        db = get_db()
+        keyfile = request.form['keyfile']
+        macaddr = request.form['macaddr']
+        checkinfo = db.execute(
+                'SELECT id, keyfile, macaddr FROM users WHERE keyfile = ?',
+                (keyfile,)).fetchone()
+        # Check the SHA256 values of keyfile and MAC address sended from client.
+        # Only allow requests with a keyfile restored in database.
+        # If the keyfile is legal but the MAC address is NULL in database, set
+        # the MAC address as the uploaded value.
+        # If the MAC address is not NULL in database, compare it with the
+        # uploaded infomation.
+        if checkinfo is None:
+            abort(403)
+        elif checkinfo['macaddr'] is None:
+            db.execute(
+                    'UPDATE users SET macaddr = ? WHERE id = ?',
+                    (macaddr, checkinfo['id']))
+            db.commit()
+        elif macaddr != checkinfo['macaddr']:
+            abort(403)
+        else:
+            pass
         upload_file = request.files['upload-file']
         filename = upload_file.filename
+        current_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
         print("filename is %s" % filename)
         if filename != '':
-            db = get_db()
             filename = unquote(filename)
-            current_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
             if db.execute('SELECT id FROM files WHERE filename = ?', (filename,)).fetchone() is None:
-                if '.' in filename:
-                    current_type = filename.rsplit('.', 1)[1].lower()
-                    if current_type in ['doc', 'docx']:
-                        current_type = 'word'
-                    elif current_type == 'pdf':
-                        current_type = 'pdf'
-                    elif current_type in ['xls', 'xlsx']:
-                        current_type = 'excel'
-                    elif current_type in ['jpg', 'jpeg', 'png', 'bmp']:
-                        current_type = 'picture'
-                    else:
-                        current_type = 'others'
+                current_type = get_type(filename)
                 db.execute(
-                        'INSERT INTO files (uploaded, filename, filetype) VALUES (?, ?, ?)',
-                        (current_time, filename, current_type))
+                        'INSERT INTO files (uploaded, userid, filename, filetype) VALUES (?, ?, ?)',
+                        (current_time, checkinfo['id'], filename, current_type))
             else:
                 db.execute(
                         'UPDATE files SET uploaded = ? WHERE filename = ?',
@@ -48,3 +65,21 @@ def upload():
         return jsonify(result)
     else:
         abort(403)
+
+# Get the type of uploaded file by its suffix
+def get_type(filename):
+    if '.' in filename:
+        res = filename.rsplit('.', 1)[1].lower()
+        if res in ['doc', 'docx']:
+            res = 'word'
+        elif res == 'pdf':
+            res = 'pdf'
+        elif res in ['xls', 'xlsx']:
+            res = 'excel'
+        elif res in ['jpg', 'jpeg', 'png', 'bmp']:
+            res = 'picture'
+        else:
+            res = 'others'
+    else:
+        res = 'others'
+    return res
